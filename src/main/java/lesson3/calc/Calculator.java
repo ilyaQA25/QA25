847c8f092a4072bb3f2f586b03257922349470f3
Ilya8633 <ilprez28@gmail.com>
2024-03-06 10:44:14 +0300

    testNG_HW_2.3_formating_resolve

--- src/main/java/lesson3/calc/Calculator.java
+++ src/main/java/lesson3/calc/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
     public int div(int a, int b){
        return a/b;
     }
+
     public double div(double a, double b) throws ArithmeticException {
        return  a/b;
     }
