3b7ca718c3030b4a57a98f0f4b276f27ec403cd5
Ilya8633 <ilprez28@gmail.com>
2024-03-11 16:25:34 +0300

    selenium_2.0

--- src/test/java/tests/MyLaminateTest.java
+++ src/test/java/tests/MyLaminateTest.java
@@ -44,7 +44,6 @@ public class MyLaminateTest {
         dieWidth.sendKeys("290");
         Thread.sleep(2000);
 
-
         WebElement dieInPackage = driver.findElement(By.id("n_packing"));
         dieInPackage.clear();
         dieInPackage.sendKeys("20");
@@ -65,11 +64,9 @@ public class MyLaminateTest {
         typeOfStowage.selectByIndex(1);
         Thread.sleep(2000);
 
-
         WebElement buttonCalculate = driver.findElement(By.id("btn_calculate"));
         buttonCalculate.click();
         Thread.sleep(2000);
-
 
         WebElement reqiredDiesresult = driver.findElement(By.xpath("//*[@id=\"calc\"]/div[1]/div[2]/div[1]"));
         Assert.assertEquals(reqiredDiesresult.getText(), "Требуемое количество плашек ламината: 95");
