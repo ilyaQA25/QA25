880442890136848a4a2b7c59fe44deffe5116da9
Ilya8633 <ilprez28@gmail.com>
2024-03-17 18:45:38 +0300

    locators_hw

--- src/test/java/tests/SauceDemo.java
+++ src/test/java/tests/SauceDemo.java
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 import services.BrowsersService;
 
 public class SauceDemo {
+
     private WebDriver driver;
 
     @BeforeMethod
@@ -23,25 +24,22 @@ public class SauceDemo {
     @Test
     public void sauceDemo() throws InterruptedException {
         driver.get(ReadProperties.getUrl());
-        Thread.sleep(2000);
-
+        Thread.sleep(1000);
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.name("password")).sendKeys("secret_sauce");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.className("submit-button")).click();
-        Thread.sleep(2000);
-
+        Thread.sleep(1000);
 
         Assert.assertTrue(driver.findElement(By.tagName("footer")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.linkText("Sauce Labs Onesie")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.partialLinkText("Onesie")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//script")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Sauce Labs Fleece Jacket']")).isDisplayed());
 
+        Assert.assertTrue(driver.findElement(By.xpath("//footer")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Sauce Labs Fleece Jacket']")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id='item_4_title_link']")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//div[contains(@id, 'roo')]")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(),'(Red)')]")).isDisplayed());
-
         Assert.assertTrue(driver.findElement(By.xpath("//footer/ancestor::div")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//footer/parent::div")).isDisplayed());
 
@@ -49,8 +47,22 @@ public class SauceDemo {
         Assert.assertTrue(driver.findElement(By.xpath("//div/div[@class='inventory_item']/following::img")).isDisplayed());
         Assert.assertTrue(driver.findElement(By.xpath("//div/div[@class='inventory_item']/preceding::img")).isDisplayed());
 
-    }
+        Assert.assertTrue(driver.findElement(By.cssSelector(".page_wrapper")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector(".btn.btn_primary")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector(".pricebar .inventory_item_price")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_1_img_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("button")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("div.inventory_item")).isDisplayed());
 
+        Assert.assertTrue(driver.findElement(By.cssSelector("[class='pricebar']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[alt~='Sauce']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[class|='bm']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[class^='inv']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("div[id$='er']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("button[data-test*='car']")).isDisplayed());
+
+
+    }
 
     @AfterMethod
     public void tearDown() {
