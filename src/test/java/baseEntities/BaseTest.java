0caf6f3c9dfcdced42ddbae3d9be136f59962e1c
Ilya8633 <ilprez28@gmail.com>
2024-04-08 18:47:31 +0300

    allure_hw_done

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -2,21 +2,39 @@ package baseEntities;
 
 import configuration.ReadProperties;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import services.BrowsersService;
+import services.WaitsService;
+import utils.InvokedListener;
 
+//@Listeners(Listener.class)
+@Listeners(InvokedListener.class)
 public class BaseTest {
     protected WebDriver driver;
+    protected WaitsService waitsService;
 
     @BeforeMethod
-    public void setup() {
+    public void setup(ITestContext iTestContext) {
         driver = new BrowsersService().getDriver();
+        this.setDriverToContext(iTestContext, driver);
+        waitsService = new WaitsService(driver);
+
         driver.get(ReadProperties.getUrl());
     }
 
     @AfterMethod
-    public void quit() {
+    public void tearDown() {
         driver.quit();
+    }
+
+    public static void setDriverToContext(ITestContext iTestContext, WebDriver driver){
+        iTestContext.setAttribute("WebDriver", driver);
+    }
+
+    public static WebDriver getDriverFromContext(ITestContext iTestContext){
+        return (WebDriver) iTestContext.getAttribute("WebDriver") ;
     }
 }
--- src/test/java/tests/OpenInventoryPage.java
+++ src/test/java/tests/OpenInventoryPage.java
@@ -2,6 +2,9 @@ package tests;
 
 import baseEntities.BaseTest;
 import configuration.ReadProperties;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.CartPage;
@@ -10,14 +13,17 @@ import steps.AddToCartSteps;
 import steps.NavigationSteps;
 
 public class OpenInventoryPage extends BaseTest {
-    @Test()
+    @Test(description = "human readable text_2")
+    @Severity(SeverityLevel.CRITICAL)
     public void openInventoryPage() {
         NavigationSteps navigationSteps = new NavigationSteps(driver);
         navigationSteps.logIn(ReadProperties.username(), ReadProperties.password());
         Assert.assertTrue(new InventoryPage(driver).isPageOpened());
     }
 
-    @Test()
+    @Test(description = "human readable text_3")
+    @Description("test adding goods in cart")
+    @Severity(SeverityLevel.NORMAL)
     public void chooseGoods() {
         NavigationSteps navigationSteps = new NavigationSteps(driver);
         navigationSteps.logIn(ReadProperties.username(), ReadProperties.password());
@@ -26,5 +32,13 @@ public class OpenInventoryPage extends BaseTest {
         addToCartSteps.addToCart();
         navigationSteps.getToCart();
         Assert.assertTrue(new CartPage(driver).isPageOpened());
+    }
+
+    @Test(description = "human readable text_2")
+    @Severity(SeverityLevel.CRITICAL)
+    public void failedOpenInventoryPage() {
+        NavigationSteps navigationSteps = new NavigationSteps(driver);
+        navigationSteps.logIn("dfgdfg", ReadProperties.password());
+        Assert.assertTrue(new InventoryPage(driver).isPageOpened());
     }
 }
