11d03306d1bcac82be7128b18338cd34bc82a67c
Ilya8633 <ilprez28@gmail.com>
2024-04-15 19:50:00 +0300

    selenide_hw

---
+++ src/test/java/tests/SelenideTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import configuration.ReadProperties;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+    @Test
+    public void simpleTest(){
+        Configuration.baseUrl = ReadProperties.getUrl();
+        open("/");
+        $(By.id("user-name")).sendKeys("standard_user");
+        $(By.id("password")).val("secret_sauce");
+        $(By.id("login-button")).click();
+        $(By.className("title")).shouldBe(visible).shouldHave(text("Products"));
+
+    }
+}
